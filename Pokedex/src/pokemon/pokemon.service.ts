260788ec6a0971fa019016a01cc4fbc99dca480f
Luis Cuya <luis.cuya@intelica.com>
2025-05-27 11:29:52 -0500

    feat : add control exception

--- Pokedex/src/pokemon/pokemon.service.ts
+++ Pokedex/src/pokemon/pokemon.service.ts
@@ -1,12 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Pokemon } from './entities/pokemon.entity';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ) {}
+
+  async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLowerCase();
-    return createPokemonDto;
+    try {
+      const pokemon = await this.pokemonModel.create(createPokemonDto);
+      return pokemon;
+    } catch (error) {
+      if(error.code === 11000) {
+        throw new BadRequestException(`Pokemon ${createPokemonDto.name} already exists`);
+      }
+      console.error(error);
+      throw new InternalServerErrorException(`Can't create Pokemon - Check server logs`);
+    }
+
   }
 
   findAll() {
