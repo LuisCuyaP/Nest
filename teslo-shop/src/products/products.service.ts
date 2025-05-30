bb83aa026e7c4d27c9e1daede47d2fdc0f112855
DevEnrqiue <luiscuyap@gmail.com>
2025-05-30 17:03:43 -0500

    feat: add search

--- teslo-shop/src/products/products.service.ts
+++ teslo-shop/src/products/products.service.ts
@@ -5,6 +5,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
 import { Repository } from 'typeorm';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { validate as isUUID } from 'uuid';
 
 @Injectable()
 export class ProductsService {
@@ -46,10 +47,16 @@ export class ProductsService {
     });    
   }
 
-  async findOne(id: string) {
-    const product = await this.productRepository.findOneBy({ id });
+  async findOne(term: string) {
+    let product: Product | null;
+    if (isUUID(term)) {
+      product = await this.productRepository.findOneBy({ id:term });
+    } else {
+      product = await this.productRepository.findOneBy({ slug: term });
+    }
+    
     if (!product) {
-      throw new InternalServerErrorException(`Product with id ${id} not found`);
+      throw new InternalServerErrorException(`Product with id ${term} not found`);
     }
     return product;
   }
