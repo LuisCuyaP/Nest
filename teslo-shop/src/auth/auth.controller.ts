dfd89c4300c816b31eb9c9554fbf52e4b110ad97
Luis Cuya <luis.cuya@intelica.com>
2025-06-03 17:54:16 -0500

    feat: decoradores endpoint testing, ver que devuelve cada decorador guards jwt

--- teslo-shop/src/auth/auth.controller.ts
+++ teslo-shop/src/auth/auth.controller.ts
@@ -1,6 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './decorators/get-user.decorator';
+import { User } from './entities/user.entity';
+import { RawHeaders } from './decorators/raw-headers.decorator';
+import { IncomingHttpHeaders } from 'http';
+
 @Controller('auth')
 export class AuthController {
   constructor(  
@@ -16,4 +22,27 @@ export class AuthController {
   loginUser(@Body() loginUserDto: LoginUserDto) {
     return this.authService.login(loginUserDto);
   }
+
+  @Get('private')
+  @UseGuards( AuthGuard() )
+  testingPrivateRoute(
+    @Req() request: Express.Request,
+    @GetUser() user: User,
+    @GetUser('email') userEmail: string,
+    
+    @RawHeaders() rawHeaders: string[],
+    //@Headers() headers: IncomingHttpHeaders,
+  ) {
+
+    //console.log({ user });
+    return {
+      ok: true,
+      message: 'Hola Mundo Private',
+      user,
+      userEmail,
+      rawHeaders
+      //headers
+    }
+  }
+
 }
--- teslo-shop/src/auth/auth.service.ts
+++ teslo-shop/src/auth/auth.service.ts
@@ -43,7 +43,8 @@ export class AuthService {
       where : { email },
       select: { 
         email: true,
-        password: true
+        password: true,
+        id: true,
       }
     });
 
