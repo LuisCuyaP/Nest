6582e7ed0f86cd29cd2c1eb2a866a46211fd32ee
Luis Cuya <luis.cuya@intelica.com>
2025-06-03 15:08:35 -0500

    feat: add createUser

--- teslo-shop/src/auth/entities/user.entity.ts
+++ teslo-shop/src/auth/entities/user.entity.ts
@@ -1,30 +1,35 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Product } from '../../products/entities';
+
 
 @Entity('users')
 export class User {
-
+    
     @PrimaryGeneratedColumn('uuid')
     id: string;
-    
+
     @Column('text', {
         unique: true
     })
     email: string;
 
-    @Column('text')
+    @Column('text', {
+        select: false
+    })
     password: string;
 
     @Column('text')
     fullName: string;
 
     @Column('bool', {
-        unique: true,
+        default: true
     })
     isActive: boolean;
 
     @Column('text', {
         array: true,
-        default: ['user'],
+        default: ['user']
     })
     roles: string[];
+
 }
