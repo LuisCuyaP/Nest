7df9cdf56518570210a4e9988a8fe22baf442fa9
Luis Cuya <luis.cuya@intelica.com>
2025-06-03 16:38:03 -0500

    feat: configuration passport authentication part I

--- teslo-shop/src/auth/auth.module.ts
+++ teslo-shop/src/auth/auth.module.ts
@@ -1,15 +1,24 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { Type } from 'class-transformer';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
+import { Module } from "@nestjs/common";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "./entities/user.entity";
+import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from '@nestjs/passport';
+
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
     TypeOrmModule.forFeature([User]),
+    PassportModule.register({defaultStrategy: 'jwt'}),
+   /*  JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { 
+        expiresIn: '2h' 
+      }
+    }) */
   ],
   exports: [TypeOrmModule]
 })
