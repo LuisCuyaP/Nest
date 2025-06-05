57733a198e0b56192057072e8041d22bf0bd99eb
Luis Cuya <luis.cuya@intelica.com>
2025-06-05 15:13:18 -0500

    feat: configuration webSocker, instalar paquete yarn add socket.io

--- teslo-shop/src/message-ws/message-ws.gateway.ts
+++ teslo-shop/src/message-ws/message-ws.gateway.ts
@@ -1,7 +1,20 @@
-import { WebSocketGateway } from '@nestjs/websockets';
-import { MessageWsService } from './message-ws.service';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from "@nestjs/websockets";
+import { Socket } from "socket.io";
+import { MessageWsService } from "./message-ws.service";
 
-@WebSocketGateway()
-export class MessageWsGateway {
-  constructor(private readonly messageWsService: MessageWsService) {}
+@WebSocketGateway({cors: true})
+export class MessageWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  
+  constructor(
+    private readonly messageWsService: MessageWsService
+  ) {}
+
+  handleConnection(client: Socket) {
+    console.log('Client connected:', client.id);
+  }
+  
+  handleDisconnect(client: Socket) {
+    console.log('Client disconnected:', client.id);
+  }
+
 }
